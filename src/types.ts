3ddb565d880acdfcf220cbaa3f606ad89ffb6b4f
Sergey Bondarenko <sbondarenko@amtoss.com.ua>
2021-11-27 13:47:13 -0600

    Hometask for lesson 2

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export type Visit = {
+    id: number;
+    timestamp: number;
+    name: string;
+}
+
+export type UserData = {
+    id: number;
+    lastname: string;
+    name: string;
+    surname: string;
+}
